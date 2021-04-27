d483a3312c901020f09a321d15ff5b957881e15a
star-algol <4nofate@gmail.com>
2021-04-26 21:04:54 -0700

    add chrome headless

---
+++ chrome.headless.conf.js
@@ -0,0 +1,25 @@
+import hooks from './config/hooks.conf';
+import base from './config/base.conf';
+
+exports.config = {
+
+    runner: 'local',
+
+    maxInstances: 1,
+    capabilities: [
+        {
+           maxInstances: 1,
+            browserName: 'chrome',
+            acceptInsecureCerts: true,
+            'goog:chromeOptions': {
+                args: ['--window-size=1280,720', '--headless', '--disable-gpu']//fix: sets the certain window size
+         },
+        }
+    ],
+
+    services: ['devtools'],
+
+    ...base,
+    ...hooks
+
+}
